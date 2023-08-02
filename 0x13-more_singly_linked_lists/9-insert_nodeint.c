e8a6b85ec05ba2aee275f8e3df67cca0b03aca3c
Kidus1209 <kidusfekadu2021@gmail.com>
2023-08-02 12:31:27 -0700

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * @idx- an index to enter new given nodes
+ * insert_nodeint_at_index - gives a new node for an index
+ * @n: information to provide for brand new nodes
+ * @head- Indicator of lists in the first node
+ *
+ * Return: Brand new node indicator
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+}
