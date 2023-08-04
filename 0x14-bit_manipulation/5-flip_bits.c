648882a7e8bba6a35e9f0724b1e7670c881f5e0f
Kidus1209 <kidusfekadu2021@gmail.com>
2023-08-04 15:01:19 -0700

    C-- Bit manipulation.

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - replaces amount of Bits that have to be changed.
+ * @m: subordinate Number
+ *
+ * @n: Initial Number.
+ * Return: The amount of Bits to swap.
+ *
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int p, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (p = 48; p >= 0; p--)
+	{
+		current = exclusive >> p;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
